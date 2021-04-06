18ded6ba854a2a4f44ee9d51dc65fbf967b44b0b
StephanieOliFerreira <stephaniessferreira@gmail.com>
2021-04-06 12:17:12 -0300

    add has_many enrollment

--- app/models/educational_institution.rb
+++ app/models/educational_institution.rb
@@ -1,4 +1,6 @@
 class EducationalInstitution < ApplicationRecord
+    has_many :enrollment
+
     validates :name, presence: true, uniqueness: true
     validates :cnpj, presence: true, uniqueness: true, numericality: { only_integer: true }
     validates :institution_type, presence: true, inclusion: { in: %w(Universidade Escola Creche) }
