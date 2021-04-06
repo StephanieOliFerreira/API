355de89923f4961fe5fcffc6bc408944d5bd4c6e
StephanieOliFerreira <stephaniessferreira@gmail.com>
2021-04-06 12:19:25 -0300

    no changes

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,5 +1,6 @@
 class Payment < ApplicationRecord
   belongs_to :enrollment
+
   validates :payment_value, presence: true
   validates :due_date, presence: true
   validates :status, presence:true, inclusion: { in: %w(Aberta Atrasada Paga) }
